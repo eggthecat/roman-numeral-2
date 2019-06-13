eab7c3d18fed1d6a992c351b5e1f375655b3566f
Dominic Montelongo and Kira Roberts <kira@>
2019-06-13 10:11:03 -0700

    Add basic html framework and js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// business logic
+
+
+// user interface logic
+$(document).ready(function(){
+  $("form#number").submit(function(event){
+    event.preventDefault();
+    var ___ = parseInt($("input#number").val());
+    var result = (_____);
+
+    $("").text(year);
+
+    $("#result").show();
+  });
+});
